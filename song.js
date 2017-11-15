dcaa6161a3fae716ab60430d832188e3306de9d7
ZomberBrand <562168176@qq.com>
2017-11-15 14:23:19 +0800

    第7次提交

--- song.js
+++ song.js
@@ -112,7 +112,7 @@ $(function() {
       $(".lyricTransform").append($p);
     });
   }
-  if(document.documentElement.clientHeight<550){
-    $('.lyric').attr('style','height:24px')
+  if(document.documentElement.clientHeight<600){
+    $('.lyric').attr('style','height:7vw')
   }
 });
